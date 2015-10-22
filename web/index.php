b5fe437f04efbb9bcddce0d46e9a7f47bd092f41
stick4real <maxime.dmt@gmail.com>
2015-10-22 18:45:09 +0200

    init project with gitignore, composer and bower + create first route with silex

---
+++ web/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// web/index.php
+require_once __DIR__.'/../vendor/autoload.php';
+
+$app = new Silex\Application();
+$app['debug'] = true;
+
+$app->get('/', function () use ($app) {
+    return 'Hello ';
+});
+
+$app->get('/hello/{name}', function ($name) use ($app) {
+    return 'Hello '.$app->escape($name);
+});
+
+$app->run();
