c91cf4c9806556fc50a85e3a0d4eea3d77dc2f75
stick4real <maxime.dmt@gmail.com>
2015-10-23 14:57:07 +0200

    init

--- web/index.php
+++ web/index.php
@@ -6,10 +6,6 @@ require_once __DIR__.'/../vendor/autoload.php';
 $app = new Silex\Application();
 $app['debug'] = true;
 
-$app->get('/', function () use ($app) {
-    return 'Hello ';
-});
-
 $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
 });
