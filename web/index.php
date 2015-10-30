57b3d7334c3fa8b632d6879de2a9c4fd4bcfc93b
stick4real <maxime.dmt@gmail.com>
2015-10-30 16:30:41 +0100

    .

--- web/index.php
+++ web/index.php
@@ -10,4 +10,8 @@ $app->get('/hello/{name}', function ($name) use ($app) {
     return 'Hello '.$app->escape($name);
 });
 
+$app->get('/', function () use ($app) {
+    return 'Hello World';
+});
+
 $app->run();
