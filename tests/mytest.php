13602fe5f7381c6113a10936ef05bc1749ff302b
stick4real <maxime.dmt@gmail.com>
2015-10-30 16:15:38 +0100

    redirect on index

---
+++ tests/mytest.php
@@ -0,0 +1,22 @@
+<?php
+
+// Class MyTest extends PHPUnit_Framework_TestCase
+// {
+// 	public function testIsTrue()
+// 	{
+// 		$this->assertTrue(true);
+// 	}
+
+// 	public function testIsEqual()
+// 	{
+// 		$this->assertEquals(4, 2+2);
+// 	}
+// }
+
+// Class MyTest extends WebTestCase
+// {
+// 	public function createApplication()
+// 	{
+		
+// 	}
+// }
